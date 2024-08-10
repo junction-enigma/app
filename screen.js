06dc94e8d247b369c0f1f7d8a59e9f0667561768
Huh Hyeongjun <hhj@devent.kr>
2024-08-11 03:27:24 +0900

    feat: add basic impl

---
+++ screen.js
@@ -0,0 +1,55 @@
+const startButton = document.getElementById('startButton')
+const stopButton = document.getElementById('stopButton')
+const video = document.querySelector('video')
+
+const vwidth = 32
+const vheight = 24
+
+startButton.addEventListener('click', () => {
+  navigator.mediaDevices.getDisplayMedia({
+    audio: true,
+    video: {
+      width: vwidth,
+      height: vheight,
+      frameRate: 60
+    }
+  }).then(stream => {
+    video.srcObject = stream
+    video.onloadedmetadata = (e) => video.play()
+  }).catch(e => console.log(e))
+})
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+// set canvas size = video size when known
+video.addEventListener('loadedmetadata', function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+});
+
+video.addEventListener('play', function() {
+  var $this = this; //cache
+  (function loop() {
+    var imgd = ctx.getImageData(0, 0, vwidth, vheight);
+    var pix = imgd.data
+    let combinePixel = 0
+
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        combinePixel += pix[i]
+    }
+
+    let resultPixel = combinePixel/pix.length
+
+    console.log(resultPixel)
+
+    if (!$this.paused && !$this.ended) {
+      ctx.drawImage($this, 0, 0);
+      setTimeout(loop, 1000 / 60); // drawing at 30fps
+    }
+  })();
+}, 0);
+
+stopButton.addEventListener('click', () => {
+  video.pause()
+})
